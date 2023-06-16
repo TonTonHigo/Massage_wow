9ff7b75904bb70cd555f32126c77c8d782f33b19
TonTonHigo <hugoh420@outlook.fr>
2023-06-16 15:22:12 +0400

    maj

---
+++ index.php
@@ -0,0 +1,98 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Massage</title>
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+        
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <header id="header_nul">
+        <nav id="menu">
+            <li class="lienmenu"><a class="lien_nul" href="#"><img id="logo" src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/04/massage_logo_header-1.png"></a></li>
+            <li class="lienmenu"><a class="lien_nul" href="#"> HOMEPAGE  </a></li>
+            <li class="lienmenu"><a class="lien_nul" href="#">ABOUT US   </a></li>
+            <li class="lienmenu"><a class="lien_nul" href="#"> PAGES & PROJECTS  </a></li>
+            <li class="lienmenu"><a class="lien_nul" href="#"> BLOG  </a></li>
+            <li class="lienmenu"> <button id="bouton_nul"> CONTACT </button>  </li>
+        </nav>
+        <div id="headbar"></div>
+    </header>
+
+    <main> 
+    <section id="sect1" >
+        <div class="section">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui perspiciatis! Impedit.</p>
+        </div>
+    </section>
+
+    </main>
+
+    
+
+    <footer>
+
+        <div id="fin_page">
+
+            <ul>
+                <li><h3>MASSAGE TYPES</h3></li>
+                <li><a href="">Sport Massage</a></li>
+                <li><a href="">Baby Massage</a></li>
+                <li><a href="">Thai Massage</a></li>
+                <li><a href="">Classic Massage</a></li>
+                <li><a href="">Physiotherapy</a></li>
+                <li><a href="">Physiotherapy</a></li>       
+            </ul>
+
+            <ul>
+                <li><h3>ABOUT US</h3></li>
+                <li><a href="">Our Masseurs</a></li>
+                <li><a href=""> Our Clients</a></li>
+                <li><a href="">Our Values</a></li>
+                <li><a href="">Photogallery</a></li>
+                <li><a href="">Our Philosophy</a></li>
+                <li><a href="">Our 2020 Goals</a></li>   
+            </ul>
+
+            <ul>
+                <li><h3>CONTACT US</h3></li>
+                <li><p>+49 8568 523 525</p><p>+43 8568 856 235</p></li>
+                <li><p>info@massage.com</p><p>hello@myspa.de</p></li>
+                <li><p>info@massage.com</p><p>helo@myspa.de</p></li>                  
+            </ul>
+
+            <ul>
+                <li><h3>SUBSCRIBE</h3></li>
+                <li><input type="text" placeholder="NAME"></li>
+                <li><input type="email" placeholder="EMAIL"></li>
+                <li><input type="submit" placeholder="SUBSCRIBE"></li>             
+            </ul>
+            
+        </div>
+        <div id="barre_blanc"></div>
+        <div>
+        </div>
+
+    </footer>
+
+
+</body>
+</html>
+
