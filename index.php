fbcdcceecaeb66ba8a94a599bbbcaca724d0ee59
TonTonHigo <hugoh420@outlook.fr>
2023-07-04 16:02:24 +0400

    update golbale

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
         <nav id="menu">
             <li class="lienmenu"><a class="lien_nul" href="#"><img id="logo" src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/04/massage_logo_header-1.png"></a></li>
             <div id="lienhead">
-                <li class="lilili"><a class="lien_nul" href="#"> HOMEPAGE v </a></li>
-                <li class="lilili"><a class="lien_nul" href="#">ABOUT US  v </a></li>
-                <li class="lilili"><a class="lien_nul" href="#"> PAGES & PROJECTS v </a></li>
-                <li class="lilili"><a class="lien_nul" href="#"> BLOG v </a></li>
+                <li class="lilili"><a class="lien_nul" href="#"> HOMEPAGE <i class="fa-solid fa-chevron-down fa-2xs" style="color: #abb8c3;"></i> </a></li>
+                <li class="lilili"><a class="lien_nul" href="#">ABOUT US  <i class="fa-solid fa-chevron-down fa-2xs" style="color: #abb8c3;"></i> </a></li>
+                <li class="lilili"><a class="lien_nul" href="#"> PAGES & PROJECTS <i class="fa-solid fa-chevron-down fa-2xs" style="color: #abb8c3;"></i> </a></li>
+                <li class="lilili"><a class="lien_nul" href="#"> BLOG <i class="fa-solid fa-chevron-down fa-2xs" style="color: #abb8c3;"></i> </a></li>
                 <li class="lilili"> <button id="bouton_nul"> CONTACT</button>  </li>
             </div>
         </nav>
@@ -28,203 +28,203 @@
     </header>
 
     <main> 
-    <section id="sect1" >
-        <div class="section">
-            <!-- WELCOME IN BEAUTY PARADISE -->
-            <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon05.png" alt="">
-            <h1>WELCOME <br>
-             IN BEAUTY PARADISE</h1>
-             <p></p>
-        </div>
-    </section>
+        <section id="sect1" >
+            <div class="section">
+                <!-- WELCOME IN BEAUTY PARADISE -->
+                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon05.png" alt="" id="logo2">
+                <h1>WELCOME <br>
+                IN BEAUTY PARADISE</h1>
+                <p></p>
+            </div>
+        </section>
 
-    <section id="sect2">
+        <section id="sect2">
 
-        <!-- what we do OUR SERVICES -->
-        <div>
+            <!-- what we do OUR SERVICES -->
             <div>
-                <p></p>
-                <p></p>
                 <div>
-                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage12.jpg" alt=""> <!-- image en fond pas en dur comme ça -->
-                    <h3></h3>
                     <p></p>
+                    <p></p>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage12.jpg" alt=""> <!-- image en fond pas en dur comme ça -->
+                        <h3></h3>
+                        <p></p>
+                    </div>
+                </div>
+                <div>
+                    <p></p>
+                    <p></p>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage33.jpg" alt=""> <!-- image en fond pas en dur comme ça -->
+                        <h3></h3>
+                        <p></p>
+                    </div>
+                </div>
+                <div>
+                    <p></p>
+                    <p></p>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage32.jpg" alt=""> <!-- image en fond pas en dur comme ça -->
+                        <h3></h3>
+                        <p></p>
+                    </div>
                 </div>
             </div>
+
+            <!-- what makes us unique WHY TO CHOOSE OUR SALON -->
             <div>
                 <p></p>
                 <p></p>
                 <div>
-                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage33.jpg" alt=""> <!-- image en fond pas en dur comme ça -->
-                    <h3></h3>
-                    <p></p>
-                </div>
-            </div>
-            <div>
-                <p></p>
-                <p></p>
-                <div>
-                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage32.jpg" alt=""> <!-- image en fond pas en dur comme ça -->
-                    <h3></h3>
-                    <p></p>
-                </div>
-            </div>
-        </div>
-
-        <!-- what makes us unique WHY TO CHOOSE OUR SALON -->
-        <div>
-            <p></p>
-            <p></p>
-            <div>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage18.png" alt="">
-            </div>
-        </div>
-
-        <!-- barre rose nul -->
-        <div>
-            <div>
-                <h1></h1>
-                <button></button>
-            </div>
-        </div>
-
-        <!-- Introducing Massage Studio -->
-        <div>
-            <div>
-                <h2></h2>
-                <hr>
-                <p></p>
-            </div>
-            <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage35-1.jpg" alt="">
-        </div>
-
-        <!-- 5 cartes  -->
-        <div>
-            <div>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon08.png" alt="">
-                <h2></h2>
-                <p></p>
-            </div>
-            <div>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon01.png" alt="">
-                <h2></h2>
-                <p></p>
-            </div>
-            <div>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon03.png" alt="">
-                <h2></h2>
-                <p></p>
-            </div>
-            <div>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon04.png" alt="">
-                <h2></h2>
-                <p></p>
-            </div>
-            <div>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon10.png" alt="">
-                <h2></h2>
-                <p></p>
-            </div>
-        </div>
-
-        <!-- You Deserve Your Time! -->
-        <div>
-            <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage39.jpg" alt="">
-            <div>
-                <h2></h2>
-                <hr>
-                <p></p>
-            </div>
-        </div>
-
-        <!-- magazine OUR BEUATY BLOG 3 cartes -->
-        <div> 
-            <div>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage21-400x250.jpg" alt="">
-                <h3></h3>
-                <p></p>
-                <button></button>
-            </div>
-            <div>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage20-400x250.jpg" alt="">
-                <h3></h3>
-                <p></p>
-                <button></button>
-            </div>
-            <div>
-                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage07-400x250.jpg" alt="">
-                <h3></h3>
-                <p></p>
-                <button></button>
-            </div>
-        </div>
-
-        <!-- meet our team OUR AWESOME MASSEURS 4 cartes -->
-        <div>
-            
-            <!-- Carte -->
-            <div> 
-                <div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
                     <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage18.png" alt="">
                 </div>
-                <h2></h2>
-                <p></p>
-                <div>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </div>
             </div>
-            <div> 
+
+            <!-- barre rose nul -->
+            <div>
                 <div>
-                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage37-1.png" alt="">
-                </div>
-                <h2></h2>
-                <p></p>
-                <div>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </div>
-            </div>
-            <div> 
-                <div>
-                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage35-1-1.png" alt="">
-                </div>
-                <h2></h2>
-                <p></p>
-                <div>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </div>
-            </div>
-            <div> 
-                <div>
-                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage38.png" alt="">
-                </div>
-                <h2></h2>
-                <p></p>
-                <div>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <h1></h1>
+                    <button></button>
                 </div>
             </div>
 
-        </div>
+            <!-- Introducing Massage Studio -->
+            <div>
+                <div>
+                    <h2></h2>
+                    <hr>
+                    <p></p>
+                </div>
+                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage35-1.jpg" alt="">
+            </div>
 
-    </section>
+            <!-- 5 cartes  -->
+            <div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon08.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon01.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon03.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon04.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon10.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- You Deserve Your Time! -->
+            <div>
+                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage39.jpg" alt="">
+                <div>
+                    <h2></h2>
+                    <hr>
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- magazine OUR BEUATY BLOG 3 cartes -->
+            <div> 
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage21-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage20-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage07-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+            </div>
+
+            <!-- meet our team OUR AWESOME MASSEURS 4 cartes -->
+            <div>
+                
+                <!-- Carte -->
+                <div> 
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage18.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div> 
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage37-1.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div> 
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage35-1-1.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div> 
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage38.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
 
     </main>
 
