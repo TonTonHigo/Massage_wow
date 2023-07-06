eed79ddd6b2a001b88e2191bf0ca5198d70fc73e
TonTonHigo <hugoh420@outlook.fr>
2023-07-06 21:11:51 +0400

    ajout de la bdd

---
+++ massage.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 06 juil. 2023 à 17:08
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.1.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `massage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+CREATE DATABASE massage;
+USE massage;
+
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id_articles` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(20) NOT NULL,
+  `resume` text NOT NULL,
+  `titre_2` varchar(20) NOT NULL,
+  `contenu` text NOT NULL,
+  `titre_3` varchar(20) NOT NULL,
+  `contenu_2` text NOT NULL,
+  `photo` varchar(100) NOT NULL,
+  `date_post` date NOT NULL,
+  `auteur` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_articles`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `articles`
+--
+
+INSERT INTO `articles` (`id_articles`, `titre`, `resume`, `titre_2`, `contenu`, `titre_3`, `contenu_2`, `photo`, `date_post`, `auteur`) VALUES
+(1, 'Best Haircut Trends ', 'cheveux wow', 'Best Haircut Trends ', 'cheveux wow deux', 'Best Haircut Trends ', 'cheveux wow trois', 'https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage21-400x250.jpg', '2023-07-06', 'Hugo'),
+(2, 'TEST', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a line in section 1.10.32.', 'TEST', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a line in section 1.10.32.', 'TEST', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a line in section 1.10.32.', 'https://massage-theme.richardpruzek.com/wp-content/uploads/2020/03/massage46.jpg', '2023-07-06', 'Jean');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
